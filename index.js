351d3cbaeeeaf38647109434b468d249741ea515
seasons986 <annalin12345@gmail.com>
2023-08-28 23:21:39 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 window.addEventListener("load", () => {
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-  }
-
   let fib = [0,1];
   let container = document.getElementById("container");
   let timer;
